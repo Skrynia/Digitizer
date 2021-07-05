66fd230da642d1870a2399c3df7937892251590f
Влад <skrynnikovsky@gmail.com>
2021-07-05 22:06:13 +0300

    Changes

--- main.py
+++ main.py
@@ -16,12 +16,13 @@ y = train_data['label']
 X = train_data[features]
 
 # create model
-model = sklearn.tree.DecisionTreeRegressor()
+model = sklearn.tree.DecisionTreeClassifier(random_state=0)
 model.fit(X, y)
 predictions = model.predict(test_data)
-predictions = predictions = numpy.around(predictions).astype(int)
 
 # output
 output = pandas.DataFrame({'ImageId': numpy.arange(1, 28001), 'Label': predictions})
 output.set_index('ImageId', inplace=True)
 output.to_csv('predictions.csv')
+
+print('All done')
